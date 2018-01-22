b13a0b2142658d868dc2bef4c579880cf7e2e363
narasimha <narasimha@codebrahma.com>
2018-01-22 17:30:57 +0530

    Added password field to user * Using bcrypt to encript password to hash, before saving in database

--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    phoneNumber: Number,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, index: { unique: true } },
+    phoneNumber: { type: Number, required: true },
+    password: { type: String, required: true }
 },{
     versionKey: false
 });
--- server/user/userHandler.js
+++ server/user/userHandler.js
@@ -5,6 +5,15 @@ const boom = require('boom')
 const httpStatus = require('http-status')
 const logger = require('../utils/logger')
 const User = require("../../models/user")
+const bcrypt = require('bcrypt')
+
+function hashPassword(password, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      return callback(err, hash);
+    });
+  });
+}
 
 const registerUser = async function (request, response) {
   const user = new User({
@@ -14,12 +23,22 @@ const registerUser = async function (request, response) {
     phoneNumber: request.payload.phoneNumber,
   });
 
-  user.save(function(error, user) {
-    if (error) {
-        console.error(error);
+  hashPassword(request.payload.password, (err, hash) => {
+    if (err) {
+      throw boom.badRequest(err);
     }
 
-    response(user);
+    user.password = hash;
+
+    // TODO: Response errors should customized and in readable format.
+    user.save((err, user) => {
+      if (err) {
+        response({error: 'Invalid details'});
+      } else {
+        response({ id: user.id, email: user.email, phoneNumber: user.phoneNumber });
+      }
+
+    });
   });
 }
 
